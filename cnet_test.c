4e13a236c96800fa97d203e2fd552c2700594406
bonedaddy <boned4ddy@protonmail.com>
2020-11-12 21:31:32 -0800

    start working on socket testing

--- cnet_test.c
+++ cnet_test.c
@@ -7,6 +7,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include "fd_pool.h"
+#include "sockets.h"
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
@@ -78,9 +79,71 @@ void test_fd_pool(void **state) {
     free_fd_pool_t(fpool);
 }
 
+void test_listen_socket(void **state) {
+    thread_logger *thl = new_thread_logger(true);
+
+    typedef struct args {
+        char *addr;
+        char *port;
+        SOCKET_OPTS sock_opts[10];
+        int num_sock_opts;
+        bool tcp;
+    } args_t;
+
+    args_t tests[4];
+    
+    tests[0].addr = "127.0.0.1";
+    tests[0].port = "5001";
+    tests[0].sock_opts[0] = REUSEADDR;
+    tests[0].sock_opts[1] = NOBLOCK;
+    tests[0].num_sock_opts = 2;
+    tests[0].tcp = true;
+
+    tests[1].addr = "127.0.0.1";
+    tests[1].port = "5002";
+    tests[1].sock_opts[0] = REUSEADDR;
+    tests[1].sock_opts[1] = BLOCK;
+    tests[1].num_sock_opts = 2;
+    tests[1].tcp = true;
+
+    tests[2].addr = "127.0.0.1";
+    tests[2].port = "5001";
+    tests[2].sock_opts[0] = REUSEADDR;
+    tests[2].sock_opts[1] = NOBLOCK;
+    tests[2].num_sock_opts = 2;
+    tests[2].tcp = false;
+
+    tests[3].addr = "127.0.0.1";
+    tests[3].port = "5002";
+    tests[3].sock_opts[0] = REUSEADDR;
+    tests[3].sock_opts[1] = BLOCK;
+    tests[3].num_sock_opts = 2;
+    tests[3].tcp = false;
+
+    for (int i = 0; i < 4; i++) {
+        // todo: enable ipv4/ipv6 selection
+        int sock_num = listen_socket(thl, tests[i].addr, tests[i].port, tests[i].tcp, true, tests[i].sock_opts, tests[i].num_sock_opts);
+        if (tests[i].tcp == false) {
+            // right now udp is unsupported so this should err
+            assert(sock_num <= 0);
+        } else {
+            assert(sock_num > 0);
+            close(sock_num);
+        }
+        LOGF_DEBUG(thl, 0, "loop %i passed", i);
+    }
+
+
+
+
+    clear_thread_logger(thl);
+}
+
+
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_fd_pool),
+        cmocka_unit_test(test_listen_socket),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
